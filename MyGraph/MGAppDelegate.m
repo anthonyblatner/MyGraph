da7ce3735ac35ef32f14912302012f3b88cc50d1
Anthony B <anthony@qubop.com>
2012-11-01 15:19:49 -0500

    Testing changes
    Testing changes description

--- MyGraph/MGAppDelegate.m
+++ MyGraph/MGAppDelegate.m
@@ -50,10 +50,6 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    
-    DLog(@"sizeof(self.points): %lu", sizeof(self.points));
-    DLog(@"sizeof(*self.points): %lu", sizeof(*self.points));
-    DLog(@"self.points[1].x: %f", (self.points)[1].x);
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
